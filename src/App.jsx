e13260fecea63f38b8df02ab7247830388d28141
MEspeaker <sho3492@gmail.com>
2025-02-26 03:55:26 +0900

    Create: admin페이지 초안UI

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import AdminLogin from './pages/AdminLogin';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import NotFound from './pages/NotFound';
+import  Admin from './pages/Admin'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -51,6 +52,10 @@ function App() {
         <Route path='/adminLogin' element={<AdminLogin />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route
+          path='/admin'
+          element={ <Admin />}
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
