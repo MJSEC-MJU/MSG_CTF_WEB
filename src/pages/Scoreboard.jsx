755f1d55503fb21835997ade255c514555d334f8
MEspeaker <sho3492@gmail.com>
2025-03-12 21:29:33 +0900

    fix: 스코어보드api연결

--- src/pages/Scoreboard.jsx
+++ src/pages/Scoreboard.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import Loading from '../components/Loading';
 import ContentBlock from '../components/Scoreboard/ContentBlock';
 import { fetchLeaderboardData } from '../api/ScoreboardApi';
@@ -9,18 +8,8 @@ const Scoreboard = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const data = await fetchLeaderboardData();
-        setDatasetsConfig(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getData();
+    fetchLeaderboardData(setDatasetsConfig);
+    setLoading(false);
   }, []);
 
   if (loading) {
@@ -48,34 +37,3 @@ const Scoreboard = () => {
 };
 
 export default Scoreboard;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-`;
-
-const GlitchText = styled.h1`
-  margin-top: 80px;
-  color: #8cff66;
-  margin-bottom: 20px;
-  text-shadow: 0 0 40px rgba(0, 255, 0, 0.8);
-  font-size: 3.5rem;
-  font-family: 'Courier New', Courier, monospace;
-  text-transform: uppercase;
-`;
-
-const NoDataText = styled.p`
-  font-size: 1.5rem;
-  color: red;
-  margin-top: 20px;
-`;
-
-const LoadingWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  padding: 20px;
-`;
