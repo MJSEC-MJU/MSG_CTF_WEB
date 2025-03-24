4bb4479e6de90efa0e12e3a58d7afcc0d779e645
MEspeaker <sho3492@gmail.com>
2025-03-24 11:16:24 +0900

    토큰재발급 백엔드에서 처리
    프론트에서 처리하고있는axios.interceptor를 주석처리하고
    SignInApi.js에서 중복되면서 쓰이고있지않은 함수인 reissueToken을 주석처리했습니다

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -11,38 +11,38 @@ const Axios = axios.create({
 });
 
 // Axios 응답 인터셉터: 401 에러 발생 시 토큰 재발급 시도
-Axios.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        console.log('리프레시 토큰으로 새 액세스 토큰 요청 중...');
-        const response = await axios.post(
-          'https://msg.mjsec.kr/api/reissue',
-          null,
-          { withCredentials: true }
-        );
-        const newAccessToken = response.headers['authorization'];
-        if (newAccessToken) {
-          console.log('액세스 토큰 갱신 성공:', newAccessToken);
-          Cookies.set('accessToken', newAccessToken, { secure: true });
-          Axios.defaults.headers.common['Authorization'] = newAccessToken;
-          originalRequest.headers['Authorization'] = newAccessToken;
-          return Axios(originalRequest);
-        }
-        throw new Error('새 액세스 토큰이 없습니다.');
-      } catch (refreshError) {
-        console.error('리프레시 토큰 만료 - 로그아웃 처리', refreshError);
-        Cookies.remove('accessToken');
-        Cookies.remove('refreshToken');
-        window.location.href = '/login'; // 강제 로그아웃 처리
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+// Axios.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       try {
+//         console.log('리프레시 토큰으로 새 액세스 토큰 요청 중...');
+//         const response = await axios.post(
+//           'https://msg.mjsec.kr/api/reissue',
+//           null,
+//           { withCredentials: true }
+//         );
+//         const newAccessToken = response.headers['authorization'];
+//         if (newAccessToken) {
+//           console.log('액세스 토큰 갱신 성공:', newAccessToken);
+//           Cookies.set('accessToken', newAccessToken, { secure: true });
+//           Axios.defaults.headers.common['Authorization'] = newAccessToken;
+//           originalRequest.headers['Authorization'] = newAccessToken;
+//           return Axios(originalRequest);
+//         }
+//         throw new Error('새 액세스 토큰이 없습니다.');
+//       } catch (refreshError) {
+//         console.error('리프레시 토큰 만료 - 로그아웃 처리', refreshError);
+//         Cookies.remove('accessToken');
+//         Cookies.remove('refreshToken');
+//         window.location.href = '/login'; // 강제 로그아웃 처리
+//         return Promise.reject(refreshError);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default Axios;
