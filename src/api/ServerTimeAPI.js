eafc3a0bd5fec6d7b46314d67ab8664ff740382b
Hanwoo <dngur515@naver.com>
2025-10-12 02:31:56 +0900

    re:add import

--- src/api/ServerTimeAPI.js
+++ src/api/ServerTimeAPI.js
@@ -1,7 +1,10 @@
+import { Axios } from './Axios';
+
 export async function fetchServerTime() {
   try {
-    const token = Cookies.get("accessToken");  // 토큰 가져오기
     const response = await Axios.get("/server-time");
+    console.log(response);
+    return response;
   } catch (e) {
     console.error('[ServerTimeAPI] fetchServerTime failed:', e);
     throw e; // 호출한 쪽에서 에러를 처리할 수 있도록 에러를 다시 던짐
