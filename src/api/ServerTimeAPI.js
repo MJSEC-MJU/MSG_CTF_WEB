dd6a7124e2261d3c6e505df6cbfd338f553342fd
Hanwoo <dngur515@naver.com>
2025-10-12 02:28:38 +0900

    I dont know

--- src/api/ServerTimeAPI.js
+++ src/api/ServerTimeAPI.js
@@ -1,16 +1,7 @@
-import { Axios } from './Axios';
-import Cookies from 'js-cookie';
-
 export async function fetchServerTime() {
   try {
     const token = Cookies.get("accessToken");  // 토큰 가져오기
-    const response = await Axios.get(`/server-time`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log("푼 문제 API 응답:", response); // 응답 데이터 확인
-    return response;
+    const response = await Axios.get("/server-time");
   } catch (e) {
     console.error('[ServerTimeAPI] fetchServerTime failed:', e);
     throw e; // 호출한 쪽에서 에러를 처리할 수 있도록 에러를 다시 던짐
