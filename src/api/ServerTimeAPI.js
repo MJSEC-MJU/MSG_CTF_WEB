74c94cfbdbf52bea42e8262b90c2dacbce51a4b5
Hanwoo <dngur515@naver.com>
2025-10-16 20:56:30 +0900

    time UTC -> KST

--- src/api/ServerTimeAPI.js
+++ src/api/ServerTimeAPI.js
@@ -2,5 +2,7 @@ import { Axios } from './Axios'; // 앞서 만든 공통 인스턴스 (baseURL =
 
 export async function fetchServerTime() {
   const { data } = await Axios.get('/server-time', { timeout: 8000 });
-  return data; // { serverTime: "..." }
+  const utcDate = new Date(data.serverTime);
+  const kstDate = new Date(utcDate.getTime() + 9 * 60 * 60 * 1000);
+  return kstDate; // { serverTime: "..." }
 }
